7af9732b27225f159fef995a035e1211282f0398
Mohammad Khoddam <mkh2097@gmail.com>
2022-01-26 23:00:38 +0330

    retrieving posts by time added

--- route/homeRounts.js
+++ route/homeRounts.js
@@ -10,32 +10,34 @@ const auth = require("../middleware/authMiddle");
 
 router.get("/", auth, async (req, res) => {
 
-    let user = await User.findOne({_id: req.session.user._id}).populate("adminOfIds memberOfIds");
-    let adminCommunities = user.adminOfIds;
-    let memberCommunities = user.memberOfIds;
-    let allCommunities = _.union(adminCommunities, memberCommunities);
-
-    let allPosts = [];
-
-    for (const c of allCommunities) {
-        let community = await Community.findOne({_id: c._id}).populate("postIds");
-
-        for (const p of community.postIds) {
-            let postOwner = await User.findOne({_id: p.postedBy});
-            debug(postOwner);
-            let communityOfPost = await Community.findOne({_id: p.community});
-            p.postedBy = postOwner;
-            p.community = communityOfPost;
+    let posts = await Post.aggregate([
+        {
+            $lookup: {
+                from: "communities",
+                localField: "community",
+                foreignField: "_id",
+                as: "postcom"
+            }
+        },
+        {$unwind: "$postcom"},
+        {
+            $lookup: {
+                from: "users",
+                localField: "postcom._id",
+                foreignField: "adminOfIds",
+                as: "comuser"
+            }
+        },
+        {$unwind: "$comuser"},
+        {$match: {$expr: {$in: ["$postcom._id", "$comuser.adminOfIds"]}}},
+        {$sort: {"createdAt": -1}},
+        {
+            $addFields: {
+                "postedBy.username": "$comuser.username",
+                "community.name": "$postcom.name"
+            }
         }
-        allPosts = _.union(allPosts, community.postIds);
-    }
-
-    return res.render("home", {myCommunities: allCommunities, hotCommunities: allCommunities, posts: allPosts});
-});
-
-
-router.get("/likes", auth, async (req, res) => {
-    let posts = await Post.find().sort({createdAt: -1})
+    ])
 
     let hotCommunities = await Community.aggregate([
         {
@@ -48,7 +50,51 @@ router.get("/likes", auth, async (req, res) => {
         {$limit: 5}
     ])
 
-    // console.log(hotCommunities)
+    return res.render("home", {hotCommunities: hotCommunities, posts: posts});
+});
+
+
+router.get("/likes", auth, async (req, res) => {
+
+    let posts = await Post.aggregate([
+        {
+            $lookup: {
+                from: "communities",
+                localField: "community",
+                foreignField: "_id",
+                as: "postcom"
+            }
+        },
+        {$unwind: "$postcom"},
+        {
+            $lookup: {
+                from: "users",
+                localField: "postcom._id",
+                foreignField: "adminOfIds",
+                as: "comuser"
+            }
+        },
+        {$unwind: "$comuser"},
+        {$match: {$expr: {$in: ["$postcom._id", "$comuser.adminOfIds"]}}},
+        {$sort: {"createdAt": -1}},
+        {
+            $addFields: {
+                "postedBy.username": "$comuser.username",
+                "community.name": "$postcom.name"
+            }
+        }
+    ])
+
+    let hotCommunities = await Community.aggregate([
+        {
+            $project: {
+                name: 1,
+                postNums: {$cond: {if: {$isArray: "$postIds"}, then: {$size: "$postIds"}, else: "NA"}}
+            }
+        },
+        {$sort: {"postNums": -1}},
+        {$limit: 5}
+    ])
 
     return res.render("home", {hotCommunities: hotCommunities, posts: posts});
 });
